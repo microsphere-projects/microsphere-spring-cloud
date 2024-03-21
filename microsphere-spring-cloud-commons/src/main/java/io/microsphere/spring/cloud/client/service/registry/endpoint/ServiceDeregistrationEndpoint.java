1995fcddf1dea3b6e2a2f73e03011b1eb039b111
小马哥（mercyblitz） <mercyblitz@gmail.com>
2024-03-21 10:50:55 +0800

    Create ServiceDeregistrationEndpoint.java

---
+++ microsphere-spring-cloud-commons/src/main/java/io/microsphere/spring/cloud/client/service/registry/endpoint/ServiceDeregistrationEndpoint.java
@@ -0,0 +1,34 @@
+package io.microsphere.spring.cloud.client.service.registry.endpoint;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
+import org.springframework.boot.actuate.endpoint.annotation.WriteOperation;
+import org.springframework.cloud.client.serviceregistry.AbstractAutoServiceRegistration;
+
+/**
+ * The {@link Endpoint @Endpoint} for Service Deregistration
+ *
+ * @author <a href="mailto:mercyblitz@gmail.com">Mercy<a/>
+ * @see AbstractAutoServiceRegistration
+ * @see Endpoint
+ * @since 1.0.0
+ */
+@Endpoint(id = "serviceDeregistration")
+public class ServiceDeregistrationEndpoint extends AbstractServiceRegistrationEndpoint {
+
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
+    @WriteOperation
+    public boolean stop() {
+        boolean isRunning = isRunning();
+        if (isRunning) {
+            serviceRegistry.deregister(registration);
+            logger.info("Service[name : '{}'] is deregistered!", applicationName);
+            setRunning(false);
+        } else {
+            logger.warn("Service[name : '{}'] is not registered, deregistration can't be executed!", applicationName);
+        }
+        return isRunning;
+    }
+}
