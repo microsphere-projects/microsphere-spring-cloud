ddba3b13e26b5d2a182ec3caa853e1718da52561
小马哥（mercyblitz） <mercyblitz@gmail.com>
2024-03-21 10:51:04 +0800

    Create ServiceRegistrationEndpointAutoConfiguration.java

---
+++ microsphere-spring-cloud-commons/src/main/java/io/microsphere/spring/cloud/client/service/registry/actuate/autoconfigure/ServiceRegistrationEndpointAutoConfiguration.java
@@ -0,0 +1,65 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.microsphere.spring.cloud.client.service.registry.actuate.autoconfigure;
+
+import io.microsphere.spring.cloud.client.service.registry.condition.ConditionalOnAutoServiceRegistrationEnabled;
+import io.microsphere.spring.cloud.client.service.registry.endpoint.ServiceDeregistrationEndpoint;
+import io.microsphere.spring.cloud.client.service.registry.endpoint.ServiceRegistrationEndpoint;
+import org.springframework.boot.actuate.autoconfigure.endpoint.condition.ConditionalOnAvailableEndpoint;
+import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.cloud.client.ConditionalOnDiscoveryEnabled;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * Microsphere {@link Endpoint @Endpoints} Auto-Configuration for Service Registration
+ *
+ * @author <a href="mailto:mercyblitz@gmail.com">Mercy<a/>
+ * @see Endpoint
+ * @since 1.0.0
+ */
+@ConditionalOnDiscoveryEnabled
+@ConditionalOnClass(name = {
+        "org.springframework.boot.actuate.endpoint.annotation.Endpoint",
+        "org.springframework.cloud.client.serviceregistry.AutoServiceRegistration"
+})
+@ConditionalOnAutoServiceRegistrationEnabled
+@AutoConfigureAfter(name = {
+        "org.springframework.cloud.client.serviceregistry.ServiceRegistryAutoConfiguration",
+        "org.springframework.cloud.zookeeper.serviceregistry.ZookeeperServiceRegistryAutoConfiguration",
+        "org.springframework.cloud.consul.serviceregistry.ConsulServiceRegistryAutoConfiguration",
+        "org.springframework.cloud.kubernetes.discovery.KubernetesDiscoveryClientAutoConfiguration",
+        "com.alibaba.cloud.nacos.registry.NacosServiceRegistryAutoConfiguration",
+})
+public class ServiceRegistrationEndpointAutoConfiguration {
+
+    @Bean
+    @ConditionalOnMissingBean
+    @ConditionalOnAvailableEndpoint
+    public ServiceRegistrationEndpoint serviceRegistrationEndpoint() {
+        return new ServiceRegistrationEndpoint();
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    @ConditionalOnAvailableEndpoint
+    public ServiceDeregistrationEndpoint serviceDeregistrationEndpoint() {
+        return new ServiceDeregistrationEndpoint();
+    }
+}
