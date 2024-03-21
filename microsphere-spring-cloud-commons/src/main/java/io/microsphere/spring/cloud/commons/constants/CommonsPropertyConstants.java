e9c184afa24571091c852db3b349f8aea244deff
小马哥（mercyblitz） <mercyblitz@gmail.com>
2024-03-21 17:35:28 +0800

    Update CommonsPropertyConstants.java

--- microsphere-spring-cloud-commons/src/main/java/io/microsphere/spring/cloud/commons/constants/CommonsPropertyConstants.java
+++ microsphere-spring-cloud-commons/src/main/java/io/microsphere/spring/cloud/commons/constants/CommonsPropertyConstants.java
@@ -31,50 +31,57 @@ import static io.microsphere.constants.PropertyConstants.MICROSPHERE_PROPERTY_NA
 public interface CommonsPropertyConstants {
 
     /**
-     * The property name prefix of Spring Cloud properties
+     * The property name prefix of Spring Cloud properties : "spring.cloud."
      */
     String SPRING_CLOUD_PROPERTY_PREFIX = "spring.cloud.";
 
     /**
-     * The property name prefix of Spring Cloud Service Registry
+     * The property name prefix of Spring Cloud Service Registry : "spring.cloud.service-registry."
      */
     String SERVICE_REGISTRY_PROPERTY_PREFIX = SPRING_CLOUD_PROPERTY_PREFIX + "service-registry.";
 
     /**
-     * The property name for Spring Cloud Service Registry Auto-Registration Feature
+     * The property name for Spring Cloud Service Registry Auto-Registration Feature :
+     * "spring.cloud.service-registry.auto-registration.enabled"
      *
      * @see AutoServiceRegistrationAutoConfiguration
      */
     String SERVICE_REGISTRY_AUTO_REGISTRATION_ENABLED_PROPERTY_NAME = SERVICE_REGISTRY_PROPERTY_PREFIX + "auto-registration." + ENABLED_PROPERTY_NAME;
 
     /**
-     * The property name for enabling Spring Cloud Features
+     * The property name for enabling Spring Cloud Features : "spring.cloud.features.enabled"
      *
      * @see CommonsClientAutoConfiguration.ActuatorConfiguration
      */
     String FEATURES_ENABLED_PROPERTY_NAME = SPRING_CLOUD_PROPERTY_PREFIX + "features." + ENABLED_PROPERTY_NAME;
 
     /**
-     * The property name prefix of Microsphere Cloud
+     * The property name prefix of Microsphere Cloud : "microsphere.spring.cloud."
      */
     String MICROSPHERE_SPRING_CLOUD_PROPERTY_NAME_PREFIX = MICROSPHERE_PROPERTY_NAME_PREFIX + SPRING_CLOUD_PROPERTY_PREFIX;
 
     /**
-     * The property name prefix of Microsphere Cloud Web MVC
+     * The property name prefix of Microsphere Cloud Web MVC : "microsphere.spring.cloud.web.mvc."
      */
     String MICROSPHERE_SPRING_CLOUD_WEB_MVC_PROPERTY_NAME_PREFIX = MICROSPHERE_SPRING_CLOUD_PROPERTY_NAME_PREFIX + "web.mvc.";
 
     /**
-     * The property name for Multiple Service Registry Enabled Feature
+     * The property name for Multiple Service Registry Enabled Feature : "microsphere.spring.cloud.multiple-registration.enabled"
      */
     String MULTIPLE_REGISTRATION_ENABLED_PROPERTY_NAME = MICROSPHERE_SPRING_CLOUD_PROPERTY_NAME_PREFIX + "multiple-registration." + ENABLED_PROPERTY_NAME;
 
+    /**
+     * The property name for Default Service Registry Type : "microsphere.spring.cloud.default-registration.type"
+     */
     String MULTIPLE_REGISTRATION_DEFAULT_REGISTRATION_PROPERTY_NAME = MICROSPHERE_SPRING_CLOUD_PROPERTY_NAME_PREFIX + "default-registration.type";
 
+    /**
+     * The property name for Default Service Registry Type : "microsphere.spring.cloud.default-service-registry.type"
+     */
     String MULTIPLE_REGISTRATION_DEFAULT_REGISTRY_PROPERTY_NAME = MICROSPHERE_SPRING_CLOUD_PROPERTY_NAME_PREFIX + "default-service-registry.type";
 
     /**
-     * The property name for Composite Service Registry Enabled Feature
+     * The property name for Composite Service Registry Enabled Feature : "microsphere.spring.cloud.composite-registration.enabled"
      */
     String COMPOSITE_REGISTRATION_ENABLED_PROPERTY_NAME = MICROSPHERE_SPRING_CLOUD_PROPERTY_NAME_PREFIX + "composite-registration." + ENABLED_PROPERTY_NAME;
 
