ac0389af8530dc330be7964db3dc0e4862cffc4a
Mercy Ma <mercyblitz@gmail.com>
2025-10-21 17:28:46 +0800

    Refactor test package structure and class modifiers
    Moved UnionDiscoveryClientIntegrationTest and UnionDiscoveryClientTest from 'discovery.autoconfigure' to 'discovery' package. Changed test classes from public to package-private. Removed unnecessary imports and updated test configuration references for improved clarity and organization.

--- microsphere-spring-cloud-commons/src/test/java/io/microsphere/spring/cloud/client/discovery/UnionDiscoveryClientTest.java
+++ microsphere-spring-cloud-commons/src/test/java/io/microsphere/spring/cloud/client/discovery/UnionDiscoveryClientTest.java
@@ -14,87 +14,65 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package io.microsphere.spring.cloud.client.discovery.autoconfigure;
+package io.microsphere.spring.cloud.client.discovery;
 
-import io.microsphere.spring.cloud.client.discovery.UnionDiscoveryClient;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.condition.EnabledIfSystemProperty;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.composite.CompositeDiscoveryClient;
+import org.springframework.cloud.client.discovery.composite.CompositeDiscoveryClientAutoConfiguration;
+import org.springframework.cloud.client.discovery.simple.SimpleDiscoveryClient;
+import org.springframework.cloud.client.discovery.simple.SimpleDiscoveryClientAutoConfiguration;
+import org.springframework.cloud.commons.util.UtilAutoConfiguration;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.testcontainers.containers.ComposeContainer;
-import org.testcontainers.junit.jupiter.EnabledIfDockerAvailable;
 
-import java.io.File;
-import java.net.URL;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.testcontainers.containers.wait.strategy.Wait.forLogMessage;
 
 /**
- * {@link UnionDiscoveryClient} Integration Test
+ * {@link UnionDiscoveryClient} Test
  *
  * @author <a href="mailto:mercyblitz@gmail.com">Mercy</a>
  * @since 1.0.0
  */
-@EnabledIfSystemProperty(named = "testcontainers.enabled", matches = "true")
-@EnabledIfDockerAvailable
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = {
-        UnionDiscoveryClientIntegrationTest.class
+        UtilAutoConfiguration.class,
+        SimpleDiscoveryClientAutoConfiguration.class,
+        CompositeDiscoveryClientAutoConfiguration.class,
+        UnionDiscoveryClient.class,
+        UnionDiscoveryClientTest.class
 })
 @TestPropertySource(
         properties = {
-                "spring.application.name=test",
-
-                "spring.cloud.service-registry.auto-registration.enabled=true",
-
-                "microsphere.spring.cloud.client.discovery.mode=union",
-
-                "microsphere.spring.cloud.multiple-registration.enabled=true"
+                "spring.cloud.discovery.client.simple.instances.test[0].instanceId=1",
+                "spring.cloud.discovery.client.simple.instances.test[0].serviceId=test",
+                "spring.cloud.discovery.client.simple.instances.test[0].host=127.0.0.1",
+                "spring.cloud.discovery.client.simple.instances.test[0].port=8080",
+                "spring.cloud.discovery.client.simple.instances.test[0].metadata.key-1=value-1"
         }
 )
-@EnableAutoConfiguration
-public class UnionDiscoveryClientIntegrationTest {
-
-    private static ComposeContainer composeContainer;
+class UnionDiscoveryClientTest {
 
     @Autowired
     private DiscoveryClient discoveryClient;
-
-    @BeforeAll
-    static void beforeAll() throws Exception {
-        ClassLoader classLoader = UnionDiscoveryClientIntegrationTest.class.getClassLoader();
-        URL resource = classLoader.getResource("META-INF/docker/service-registry-servers.yml");
-        File dockerComposeFile = new File(resource.toURI());
-        composeContainer = new ComposeContainer(dockerComposeFile);
-        composeContainer.waitingFor("nacos", forLogMessage(".*Nacos started successfully.*", 1))
-                .waitingFor("eureka", forLogMessage(".*Started EurekaServerApplication.*", 1))
-                .start();
-    }
-
-    @AfterAll
-    static void afterAll() {
-        composeContainer.stop();
-    }
 
     @Test
     void test() {
         assertEquals(CompositeDiscoveryClient.class, discoveryClient.getClass());
         CompositeDiscoveryClient compositeDiscoveryClient = CompositeDiscoveryClient.class.cast(discoveryClient);
         List<DiscoveryClient> discoveryClients = compositeDiscoveryClient.getDiscoveryClients();
-        assertEquals(6, discoveryClients.size());
+        assertEquals(2, discoveryClients.size());
         assertEquals(UnionDiscoveryClient.class, discoveryClients.get(0).getClass());
+        assertEquals(SimpleDiscoveryClient.class, discoveryClients.get(1).getClass());
         List<String> services = compositeDiscoveryClient.getServices();
-        assertTrue(services.size() > 1);
+        assertEquals(Arrays.asList("test"), services);
+        assertEquals(services, discoveryClients.get(0).getServices());
+        assertEquals(services, discoveryClients.get(1).getServices());
     }
 }
--- microsphere-spring-cloud-commons/src/test/java/io/microsphere/spring/cloud/client/service/registry/endpoint/ServiceRegistrationEndpointTest.java
+++ microsphere-spring-cloud-commons/src/test/java/io/microsphere/spring/cloud/client/service/registry/endpoint/ServiceRegistrationEndpointTest.java
@@ -14,39 +14,32 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package io.microsphere.spring.cloud.client.discovery;
 
+package io.microsphere.spring.cloud.client.service.registry.endpoint;
+
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.cloud.client.discovery.composite.CompositeDiscoveryClient;
 import org.springframework.cloud.client.discovery.composite.CompositeDiscoveryClientAutoConfiguration;
-import org.springframework.cloud.client.discovery.simple.SimpleDiscoveryClient;
 import org.springframework.cloud.client.discovery.simple.SimpleDiscoveryClientAutoConfiguration;
 import org.springframework.cloud.commons.util.UtilAutoConfiguration;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import java.util.Arrays;
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 /**
- * {@link UnionDiscoveryClient} Test
+ * {@link ServiceRegistrationEndpoint} Test
  *
  * @author <a href="mailto:mercyblitz@gmail.com">Mercy</a>
+ * @see ServiceRegistrationEndpoint
  * @since 1.0.0
  */
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = {
         UtilAutoConfiguration.class,
         SimpleDiscoveryClientAutoConfiguration.class,
-        CompositeDiscoveryClientAutoConfiguration.class,
-        UnionDiscoveryClient.class,
-        UnionDiscoveryClientTest.class
+        CompositeDiscoveryClientAutoConfiguration.class
 })
 @TestPropertySource(
         properties = {
@@ -57,22 +50,17 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
                 "spring.cloud.discovery.client.simple.instances.test[0].metadata.key-1=value-1"
         }
 )
-public class UnionDiscoveryClientTest {
+class ServiceRegistrationEndpointTest {
 
-    @Autowired
-    private DiscoveryClient discoveryClient;
+    @BeforeEach
+    void setUp() {
+    }
 
     @Test
-    void test() {
-        assertEquals(CompositeDiscoveryClient.class, discoveryClient.getClass());
-        CompositeDiscoveryClient compositeDiscoveryClient = CompositeDiscoveryClient.class.cast(discoveryClient);
-        List<DiscoveryClient> discoveryClients = compositeDiscoveryClient.getDiscoveryClients();
-        assertEquals(2, discoveryClients.size());
-        assertEquals(UnionDiscoveryClient.class, discoveryClients.get(0).getClass());
-        assertEquals(SimpleDiscoveryClient.class, discoveryClients.get(1).getClass());
-        List<String> services = compositeDiscoveryClient.getServices();
-        assertEquals(Arrays.asList("test"), services);
-        assertEquals(services, discoveryClients.get(0).getServices());
-        assertEquals(services, discoveryClients.get(1).getServices());
+    void testMetadata() {
+    }
+
+    @Test
+    void testStart() {
     }
 }
