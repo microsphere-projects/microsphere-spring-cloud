23a216bf7a84671d97ffdca1fe6f06f6227707c4
Mercy Ma <mercyblitz@gmail.com>
2025-10-23 11:51:49 +0800

    Refactor WebMvcServiceRegistryAutoConfiguration class
    Simplified the class by removing unused imports, annotations, and methods. Refactored exclusion logic for endpoint mappings and moved shared logic to the superclass WebServiceRegistryAutoConfiguration for better maintainability.

--- microsphere-spring-cloud-commons/src/main/java/io/microsphere/spring/cloud/client/service/registry/autoconfigure/WebMvcServiceRegistryAutoConfiguration.java
+++ microsphere-spring-cloud-commons/src/main/java/io/microsphere/spring/cloud/client/service/registry/autoconfigure/WebMvcServiceRegistryAutoConfiguration.java
@@ -16,34 +16,20 @@
  */
 package io.microsphere.spring.cloud.client.service.registry.autoconfigure;
 
-import io.microsphere.logging.Logger;
-import io.microsphere.spring.cloud.client.service.registry.condition.ConditionalOnAutoServiceRegistrationEnabled;
-import io.microsphere.spring.web.event.WebEndpointMappingsReadyEvent;
 import io.microsphere.spring.web.metadata.WebEndpointMapping;
 import io.microsphere.util.ValueHolder;
 import jakarta.servlet.Filter;
 import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.AutoConfigureAfter;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.boot.autoconfigure.web.servlet.DispatcherServletRegistrationBean;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.cloud.client.serviceregistry.Registration;
 import org.springframework.cloud.client.serviceregistry.ServiceRegistry;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.event.EventListener;
 
 import java.util.Collection;
-import java.util.HashSet;
-import java.util.Iterator;
 import java.util.Objects;
-import java.util.Set;
 
-import static io.microsphere.logging.LoggerFactory.getLogger;
-import static io.microsphere.spring.cloud.client.service.util.ServiceInstanceUtils.attachMetadata;
 import static io.microsphere.util.ArrayUtils.EMPTY_STRING_ARRAY;
 import static io.microsphere.util.ArrayUtils.arrayEquals;
 import static java.lang.Boolean.FALSE;
@@ -55,28 +41,10 @@ import static org.springframework.boot.autoconfigure.condition.ConditionalOnWebA
  * @author <a href="mailto:mercyblitz@gmail.com">Mercy</a>
  * @since 1.0.0
  */
-@Configuration(proxyBeanMethods = false)
-@ConditionalOnClass(name = {
-        "io.microsphere.spring.web.metadata.WebEndpointMapping",
-        "io.microsphere.spring.web.event.WebEndpointMappingsReadyEvent"
-})
-@ConditionalOnBean(Registration.class)
 @ConditionalOnWebApplication(type = SERVLET)
-@ConditionalOnAutoServiceRegistrationEnabled
-@AutoConfigureAfter(value = {
-        ServiceRegistryAutoConfiguration.class
-})
-public class WebMvcServiceRegistryAutoConfiguration {
-
-    private static final Logger logger = getLogger(WebMvcServiceRegistryAutoConfiguration.class);
+public class WebMvcServiceRegistryAutoConfiguration extends WebServiceRegistryAutoConfiguration {
 
     private static final String[] DEFAULT_URL_MAPPINGS = {"/*"};
-
-    @Autowired
-    private Registration registration;
-
-    @Value("${management.endpoints.web.base-path:/actuator}")
-    private String actuatorBasePath;
 
     @Value("${server.servlet.context-path:}")
     private String contextPath;
@@ -87,32 +55,14 @@ public class WebMvcServiceRegistryAutoConfiguration {
     @Autowired
     private ObjectProvider<DispatcherServletRegistrationBean> dispatcherServletRegistrationBeanProvider;
 
-    @EventListener(WebEndpointMappingsReadyEvent.class)
-    public void onApplicationEvent(WebEndpointMappingsReadyEvent event) {
-        Collection<WebEndpointMapping> webEndpointMappings = event.getMappings();
-        attachWebMappingsMetadata(registration, webEndpointMappings);
+    @Override
+    protected String getContextPath() {
+        return this.contextPath;
     }
 
-    private void attachWebMappingsMetadata(Registration registration, Collection<WebEndpointMapping> webEndpointMappings) {
-        Set<WebEndpointMapping> mappings = new HashSet<>(webEndpointMappings);
-        excludeMappings(mappings);
-        attachMetadata(contextPath, registration, mappings);
-    }
-
-    private void excludeMappings(Set<WebEndpointMapping> mappings) {
-        Iterator<WebEndpointMapping> iterator = mappings.iterator();
-        while (iterator.hasNext()) {
-            WebEndpointMapping mapping = iterator.next();
-            String[] patterns = mapping.getPatterns();
-            if (isBuiltInFilterMapping(patterns)
-                    || isDispatcherServletMapping(mapping, patterns)
-                    || isActuatorWebEndpointMapping(patterns)
-            ) {
-                logger.trace("The '{}' was removed", mapping);
-                iterator.remove();
-            }
-
-        }
+    @Override
+    protected boolean isExcludedMapping(WebEndpointMapping mapping, String[] patterns) {
+        return isBuiltInFilterMapping(patterns) || isDispatcherServletMapping(mapping, patterns);
     }
 
     private boolean isBuiltInFilterMapping(String[] patterns) {
@@ -143,16 +93,6 @@ public class WebMvcServiceRegistryAutoConfiguration {
         return found.getValue();
     }
 
-
-    private boolean isActuatorWebEndpointMapping(String[] patterns) {
-        for (String pattern : patterns) {
-            if (pattern.startsWith(actuatorBasePath)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
     private boolean matchFilter(FilterRegistrationBean filterRegistrationBean, String[] patterns) {
         Collection<String> urlPatterns = filterRegistrationBean.getUrlPatterns();
         return matchUrlPatterns(urlPatterns, patterns);
@@ -162,5 +102,4 @@ public class WebMvcServiceRegistryAutoConfiguration {
         String[] urlPatternsArray = urlPatterns.isEmpty() ? DEFAULT_URL_MAPPINGS : urlPatterns.toArray(EMPTY_STRING_ARRAY);
         return arrayEquals(urlPatternsArray, patterns);
     }
-
 }
