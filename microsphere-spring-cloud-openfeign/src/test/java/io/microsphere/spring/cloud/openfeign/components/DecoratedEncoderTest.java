01adce0990cb5eba270ffd16c009d35a971d1040
Mercy Ma <mercyblitz@gmail.com>
2025-10-24 16:49:42 +0800

    Add unit tests for DecoratedEncoder component
    Introduces DecoratedEncoderTest to verify the behavior of the DecoratedEncoder class, including component type resolution and encoding functionality.

---
+++ microsphere-spring-cloud-openfeign/src/test/java/io/microsphere/spring/cloud/openfeign/components/DecoratedEncoderTest.java
@@ -0,0 +1,83 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.microsphere.spring.cloud.openfeign.components;
+
+
+import feign.RequestTemplate;
+import feign.codec.Encoder;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectFactory;
+import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
+import org.springframework.cloud.openfeign.support.SpringEncoder;
+
+import static io.microsphere.spring.cloud.openfeign.components.DecoratedFeignComponent.instantiate;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertSame;
+
+/**
+ * {@link DecoratedEncoder} Test
+ *
+ * @author <a href="mailto:mercyblitz@gmail.com">Mercy</a>
+ * @see DecoratedEncoder
+ * @since 1.0.0
+ */
+class DecoratedEncoderTest extends DecoratedFeignComponentTest {
+
+    private Encoder delegate;
+
+    private DecoratedEncoder decoratedEncoder;
+
+    @BeforeEach
+    void setUp() {
+        super.setUp();
+        HttpMessageConverters httpMessageConverters = new HttpMessageConverters();
+        ObjectFactory<HttpMessageConverters> messageConverters = () -> httpMessageConverters;
+        this.delegate = new SpringEncoder(messageConverters);
+        this.decoratedEncoder = instantiate(DecoratedEncoder.class, Encoder.class, contextId, contextFactory,
+                clientProperties, delegate);
+    }
+
+    @Test
+    void testComponentTypeFromDefaultConfiguration() {
+        initDefaultConfiguration();
+        this.decoratedEncoder.getDefaultConfiguration().setEncoder((Class) this.delegate.getClass());
+        assertSame(this.delegate.getClass(), this.decoratedEncoder.componentType());
+    }
+
+    @Test
+    void testComponentTypeFromCurrentConfiguration() {
+        initCurrentConfiguration();
+        this.decoratedEncoder.getCurrentConfiguration().setEncoder((Class) this.delegate.getClass());
+        assertSame(this.delegate.getClass(), this.decoratedEncoder.componentType());
+    }
+
+    @Test
+    void testComponentType() {
+        assertSame(Encoder.class, this.decoratedEncoder.componentType());
+    }
+
+    @Test
+    void testEncode() {
+        RequestTemplate template = new RequestTemplate();
+        String value = "Test";
+        this.decoratedEncoder.encode(value, String.class, template);
+        byte[] body = template.body();
+        assertArrayEquals(value.getBytes(template.requestCharset()), body);
+    }
+}
