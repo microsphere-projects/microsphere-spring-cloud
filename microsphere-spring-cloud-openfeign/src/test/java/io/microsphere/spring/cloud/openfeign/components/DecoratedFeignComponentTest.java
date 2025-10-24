9c79e48abdf9c0e6f7732eb2cf053496b6c9b34c
Mercy Ma <mercyblitz@gmail.com>
2025-10-24 13:45:15 +0800

    Add abstract test for DecoratedFeignComponent
    Introduces DecoratedFeignComponentTest as an abstract base class for testing DecoratedFeignComponent. Sets up common test fields and initialization for Feign client context and properties.

---
+++ microsphere-spring-cloud-openfeign/src/test/java/io/microsphere/spring/cloud/openfeign/components/DecoratedFeignComponentTest.java
@@ -0,0 +1,47 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.microsphere.spring.cloud.openfeign.components;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.cloud.context.named.NamedContextFactory;
+import org.springframework.cloud.openfeign.FeignClientFactory;
+import org.springframework.cloud.openfeign.FeignClientProperties;
+import org.springframework.cloud.openfeign.FeignClientSpecification;
+
+/**
+ * Abstract {@link DecoratedFeignComponent} Test
+ *
+ * @author <a href="mailto:mercyblitz@gmail.com">Mercy</a>
+ * @see DecoratedFeignComponent
+ * @since 1.0.0
+ */
+abstract class DecoratedFeignComponentTest {
+
+    protected String contextId;
+
+    protected NamedContextFactory<FeignClientSpecification> contextFactory;
+
+    protected FeignClientProperties clientProperties;
+
+    @BeforeEach
+    void setUp() {
+        this.contextId = "default";
+        this.contextFactory = new FeignClientFactory();
+        this.clientProperties = new FeignClientProperties();
+    }
+}
