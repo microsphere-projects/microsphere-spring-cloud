9d2497be8e1c73399a7cae01c5b4714727e4d0d5
Mercy Ma <mercyblitz@gmail.com>
2025-11-07 21:50:28 +0800

    Update ConditionalOnPropertyEnabledTest.java

--- microsphere-spring-cloud-commons/src/test/java/io/microsphere/spring/cloud/test/ConditionalOnPropertyEnabledTest.java
+++ microsphere-spring-cloud-commons/src/test/java/io/microsphere/spring/cloud/test/ConditionalOnPropertyEnabledTest.java
@@ -17,7 +17,6 @@
 
 package io.microsphere.spring.cloud.test;
 
-import io.microsphere.util.ArrayUtils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
