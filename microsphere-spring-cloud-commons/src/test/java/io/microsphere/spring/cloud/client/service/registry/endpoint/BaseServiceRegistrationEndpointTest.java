ecbe9f8798eb0d58391a6b7f891eb0a6b000ac9f
Mercy Ma <mercyblitz@gmail.com>
2026-03-27 10:58:59 +0800

    Remove SpringLoggingTest annotation from test
    Delete the unused import and the @SpringLoggingTest annotation from BaseServiceRegistrationEndpointTest, cleaning up the test class by removing the SpringLoggingTest extension usage.

--- microsphere-spring-cloud-commons/src/test/java/io/microsphere/spring/cloud/client/service/registry/endpoint/BaseServiceRegistrationEndpointTest.java
+++ microsphere-spring-cloud-commons/src/test/java/io/microsphere/spring/cloud/client/service/registry/endpoint/BaseServiceRegistrationEndpointTest.java
@@ -18,7 +18,6 @@
 package io.microsphere.spring.cloud.client.service.registry.endpoint;
 
 
-import io.microsphere.spring.test.junit.jupiter.SpringLoggingTest;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -49,7 +48,6 @@ import static org.springframework.boot.test.context.SpringBootTest.WebEnvironmen
         },
         webEnvironment = RANDOM_PORT
 )
-@SpringLoggingTest
 class BaseServiceRegistrationEndpointTest {
 
     @Autowired
