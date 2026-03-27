ca479b991635b15ded3bf88f838d2ece84c7ce76
Mercy Ma <mercyblitz@gmail.com>
2026-03-27 11:02:27 +0800

    Reorder static import in AQueryMapEncoder
    Move the static import of LoggerFactory.getLogger to be grouped with other imports and add a blank line for improved import formatting in the test AQueryMapEncoder. No functional changes; only import ordering/formatting.

--- microsphere-spring-cloud-openfeign/src/test/java/io/microsphere/spring/cloud/openfeign/querymapencoder/AQueryMapEncoder.java
+++ microsphere-spring-cloud-openfeign/src/test/java/io/microsphere/spring/cloud/openfeign/querymapencoder/AQueryMapEncoder.java
@@ -2,10 +2,11 @@ package io.microsphere.spring.cloud.openfeign.querymapencoder;
 
 import feign.QueryMapEncoder;
 import io.microsphere.logging.Logger;
-import static io.microsphere.logging.LoggerFactory.getLogger;
 
 import java.util.HashMap;
 import java.util.Map;
+
+import static io.microsphere.logging.LoggerFactory.getLogger;
 
 /**
  * @author <a href="mailto:maimengzzz@gmail.com">韩超</a>
