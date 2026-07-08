1272fe91a2d1272fc04c7b658f776c349a484e6e
Mercy Ma <mercyblitz@gmail.com>
2026-07-08 20:55:57 +0800

    Add integration test for service endpoints
    Introduce a Spring Boot integration test for `ServiceRegistrationEndpointAutoConfiguration` that starts a random-port context with endpoint and auto-registration properties enabled, then asserts both `ServiceRegistrationEndpoint` and `ServiceDeregistrationEndpoint` beans are available.

---
+++ microsphere-spring-cloud-commons/src/test/java/io/microsphere/spring/cloud/client/service/registry/actuate/autoconfigure/ServiceRegistrationEndpointAutoConfigurationIntergrationTest.java
@@ -0,0 +1,46 @@
+package io.microsphere.spring.cloud.client.service.registry.actuate.autoconfigure;
+
+import io.microsphere.spring.cloud.client.service.registry.endpoint.ServiceDeregistrationEndpoint;
+import io.microsphere.spring.cloud.client.service.registry.endpoint.ServiceRegistrationEndpoint;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+/**
+ * {@link ServiceRegistrationEndpointAutoConfiguration} Intergration Test
+ *
+ * @author <a href="mailto:mercyblitz@gmail.com">Mercy<a/>
+ * @see ServiceRegistrationEndpointAutoConfiguration
+ * @since 1.0.0
+ */
+@SpringBootTest(
+        classes = {
+                ServiceRegistrationEndpointAutoConfigurationIntergrationTest.class
+        },
+        webEnvironment = RANDOM_PORT,
+        properties = {
+                "microsphere.spring.cloud.service-registry.auto-registration.simple.enabled=true",
+                "management.endpoint.serviceRegistration.enabled=true",
+                "management.endpoint.serviceDeregistration.enabled=true",
+        }
+)
+@EnableAutoConfiguration
+class ServiceRegistrationEndpointAutoConfigurationIntergrationTest {
+
+    @Autowired
+    private ObjectProvider<ServiceRegistrationEndpoint> serviceRegistrationEndpoint;
+
+    @Autowired
+    private ObjectProvider<ServiceDeregistrationEndpoint> serviceDeregistrationEndpoint;
+
+    @Test
+    void testEndpoints() {
+        assertNotNull(serviceRegistrationEndpoint.getIfAvailable());
+        assertNotNull(serviceDeregistrationEndpoint.getIfAvailable());
+    }
+}
