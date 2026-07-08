c62b5fee887b896144eee740ec3b7af886220fb4
Mercy Ma <mercyblitz@gmail.com>
2026-07-08 20:52:40 +0800

    Add auto-config test for service endpoints
    Introduce `ServiceRegistrationEndpointAutoConfigurationTest` to validate actuator endpoint auto-configuration behavior for service registration and deregistration. The test configures endpoint exposure/enabled properties and asserts conditional behavior when discovery, auto-registration, or required classes are unavailable.

---
+++ microsphere-spring-cloud-commons/src/test/java/io/microsphere/spring/cloud/client/service/registry/actuate/autoconfigure/ServiceRegistrationEndpointAutoConfigurationTest.java
@@ -0,0 +1,66 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.microsphere.spring.cloud.client.service.registry.actuate.autoconfigure;
+
+
+import io.microsphere.spring.boot.test.AutoConfigurationTest;
+import io.microsphere.spring.cloud.client.service.registry.endpoint.ServiceDeregistrationEndpoint;
+import io.microsphere.spring.cloud.client.service.registry.endpoint.ServiceRegistrationEndpoint;
+import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.client.serviceregistry.AutoServiceRegistration;
+
+import java.util.Set;
+
+/**
+ * {@link ServiceRegistrationEndpointAutoConfiguration} Test
+ *
+ * @author <a href="mailto:mercyblitz@gmail.com">Mercy</a>
+ * @see ServiceRegistrationEndpointAutoConfiguration
+ * @since 1.0.0
+ */
+@SpringBootTest(
+        classes = {
+                ServiceRegistrationEndpointAutoConfigurationTest.class
+        },
+        properties = {
+                "management.endpoints.web.exposure.include=serviceRegistration,serviceDeregistration",
+                "management.endpoint.serviceRegistration.enabled=true",
+                "management.endpoint.serviceDeregistration.enabled=true",
+        }
+)
+class ServiceRegistrationEndpointAutoConfigurationTest extends AutoConfigurationTest<ServiceRegistrationEndpointAutoConfiguration> {
+
+    @Override
+    protected void configureAutoConfiguredClasses(Set<Class<?>> autoConfiguredClasses) {
+        autoConfiguredClasses.add(ServiceRegistrationEndpoint.class);
+        autoConfiguredClasses.add(ServiceDeregistrationEndpoint.class);
+    }
+
+    @Override
+    protected void configureGlobalDisabledPropertyValues(Set<String> globalDisabledPropertyValues) {
+        globalDisabledPropertyValues.add("spring.cloud.discovery.enabled=false");
+        globalDisabledPropertyValues.add("spring.cloud.service-registry.auto-registration.enabled=false");
+    }
+
+    @Override
+    protected void configureGlobalMissingClasses(Set<Class<?>> globalMissingClasses) {
+        globalMissingClasses.add(Endpoint.class);
+        globalMissingClasses.add(AutoServiceRegistration.class);
+    }
+}
