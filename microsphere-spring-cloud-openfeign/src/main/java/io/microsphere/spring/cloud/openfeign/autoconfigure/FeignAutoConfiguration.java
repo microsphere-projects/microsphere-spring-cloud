4bd643e99b689ff747ac97b511209504a540bbd4
Mercy Ma <mercyblitz@gmail.com>
2026-07-08 23:10:46 +0800

    Use shared OpenFeign availability condition
    Refactor `FeignAutoConfiguration` to use `@ConditionalOnOpenFeignAvailable` instead of a local `@ConditionalOnProperty` flag. This removes the module-specific enabled property constant and related metadata imports, and simplifies classpath checks to only require Spring Cloud OpenFeign APIs.

--- microsphere-spring-cloud-openfeign/src/main/java/io/microsphere/spring/cloud/openfeign/autoconfigure/FeignAutoConfiguration.java
+++ microsphere-spring-cloud-openfeign/src/main/java/io/microsphere/spring/cloud/openfeign/autoconfigure/FeignAutoConfiguration.java
@@ -1,19 +1,13 @@
 package io.microsphere.spring.cloud.openfeign.autoconfigure;
 
-import io.microsphere.annotation.ConfigurationProperty;
-import io.microsphere.constants.PropertyConstants;
 import io.microsphere.spring.cloud.openfeign.components.NoOpRequestInterceptor;
+import io.microsphere.spring.cloud.openfeign.condition.ConditionalOnOpenFeignAvailable;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.cloud.openfeign.FeignBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import static io.microsphere.annotation.ConfigurationProperty.APPLICATION_SOURCE;
-import static io.microsphere.constants.SymbolConstants.DOT;
-import static io.microsphere.spring.cloud.commons.constants.CommonsPropertyConstants.MICROSPHERE_SPRING_CLOUD_PROPERTY_NAME_PREFIX;
-import static io.microsphere.spring.cloud.openfeign.autoconfigure.FeignAutoConfiguration.ENABLED_PROPERTY_NAME;
 import static io.microsphere.spring.cloud.openfeign.components.NoOpRequestInterceptor.INSTANCE;
 
 /**
@@ -23,25 +17,14 @@ import static io.microsphere.spring.cloud.openfeign.components.NoOpRequestInterc
  * @author <a href="mailto:mercyblitz@gmail.com">Mercy</a>
  */
 @Configuration(proxyBeanMethods = false)
-@ConditionalOnProperty(value = ENABLED_PROPERTY_NAME, matchIfMissing = true)
+@ConditionalOnOpenFeignAvailable
 @ConditionalOnClass(name = {
-        "feign.Feign",                                                  // OpenFeign Core API
         "org.springframework.cloud.openfeign.FeignBuilderCustomizer"    // Spring Cloud OpenFeign API
 })
 @AutoConfigureAfter(name = {
         "org.springframework.cloud.openfeign.FeignAutoConfiguration"
 })
 public class FeignAutoConfiguration {
-
-    /**
-     * The property name for enabling {@link FeignAutoConfiguration} : "microsphere.spring.cloud.openfeign.enabled"
-     */
-    @ConfigurationProperty(
-            type = boolean.class,
-            defaultValue = "true",
-            source = APPLICATION_SOURCE
-    )
-    public static final String ENABLED_PROPERTY_NAME = MICROSPHERE_SPRING_CLOUD_PROPERTY_NAME_PREFIX + "openfeign" + DOT + PropertyConstants.ENABLED_PROPERTY_NAME;
 
     /**
      * Creates a {@link FeignBuilderCustomizer} that adds the {@link NoOpRequestInterceptor}
